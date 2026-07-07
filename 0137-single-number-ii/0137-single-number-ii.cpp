f02387e7a3067af727c9a1a5229712d6db256028
adarshx64-netizen <adarshx64@gmail.com>
2026-07-07 10:21:54 +0530

    Time: 1 ms (43.54%), Space: 13.4 MB (34.24%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,17 +1,56 @@
 class Solution {
 public:
+
+
+    // int optimal(vector<int>& nums, int n) {
+
+
+    // }
+
+    // TC -> O(n logn + n/3)
+    // SC -> O(1)
+    int better2(vector<int>& nums, int n) {
+        sort(nums.begin(), nums.end());
+
+        for(int i = 1; i < n; i += 3) {
+            if(nums[i] != nums[i - 1]) return nums[i - 1];
+        }
+         
+        return nums[n - 1];
+    }
+
+
+    // TC -> O(32 * n) (always)
+    // SC -> O(1)
+    int better(vector<int>& nums, int n) {
+
+        int ans = 0;
+        for(int bitIndex = 0; bitIndex <= 31; bitIndex++){
+
+            int cnt = 0;
+            for(int i = 0; i < n; i++) {
+
+                // Check bitIndex is set or not
+                if(nums[i] & (1 << bitIndex)) cnt++;
+            }
+
+            // If cnt is not multiple of 3 then set bitIndex bit
+            if(cnt % 3) {
+                ans |= (1 << bitIndex);
+            }
+        }
+
+        return ans;
+    }
+
+
+    
+
     int singleNumber(vector<int>& nums) {
         int n = nums.size();
 
-        sort(nums.begin(), nums.end());
-
-        if(n == 1 || nums[0] != nums[1]) return nums[0];
-
-        for(int i = 1; i < n-1; i++) {
-
-            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) return nums[i];
-        }
-
-        return nums[n - 1];
+        
+        // return better(nums, n);
+        return better2(nums, n);
     }
 };
