d2c80c9dfc9afc69c28bb6ce10761b054026156a
adarshx64-netizen <adarshx64@gmail.com>
2026-07-13 08:22:25 +0530

    Time: 0 ms (100%), Space: 12.6 MB (76.15%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -5,14 +5,30 @@ public:
         unordered_map<int, int> mpp;
         stack<int> st;
 
-        for(int i = nums2.size() - 1; i >= 0; i--) {
-            while(!st.empty() && st.top() <= nums2[i]) st.pop();
-
-            if(st.empty()) mpp[nums2[i]] = -1;
-            else mpp[nums2[i]] = st.top();
-
-            st.push(nums2[i]);
+    // Left to Right
+        for(int x : nums2) {
+            while(!st.empty() && x > st.top()) {
+                mpp[st.top()] = x;
+                st.pop();
+            }
+            st.push(x);
         }
+
+        while(!st.empty()) {
+            mpp[st.top()] = -1;
+            st.pop();
+        }
+
+
+    // Right to Left
+        // for(int i = nums2.size() - 1; i >= 0; i--) {
+        //     while(!st.empty() && st.top() <= nums2[i]) st.pop();
+
+        //     if(st.empty()) mpp[nums2[i]] = -1;
+        //     else mpp[nums2[i]] = st.top();
+
+        //     st.push(nums2[i]);
+        // }
 
         for(int x : nums1) ans.push_back(mpp[x]);
     }
