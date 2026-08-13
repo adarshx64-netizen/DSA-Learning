be109967d1afac4671aebbba47f516f2b72c4d2a
adarshx64-netizen <adarshx64@gmail.com>
2026-08-13 21:32:11 +0530

    Time: 24 ms (49.16%), Space: 23.6 MB (36.47%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -11,11 +11,30 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+
+        // BF Approach
+        // return lca(root, p, q);
+
+        // Optimal
+        // TC -> O(H)
+        // SC -> O()
+        if(root == NULL) return root;
+
+        // if both of them are on left then go to the left
+        if(root->val > p->val && root->val > q->val) return lowestCommonAncestor(root->left, p, q);
         
+        // if both of them are on right then go to the right
+        if(root->val < p->val && root->val < q->val) return lowestCommonAncestor(root->right, p, q);
+
+        return root;
+    }
+
+    TreeNode* lca(TreeNode* root, TreeNode* p, TreeNode* q) {
+
         if(root == NULL || root == p || root == q) return root;
 
-        TreeNode* left = lowestCommonAncestor(root->left, p, q);
-        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        TreeNode* left = lca(root->left, p, q);
+        TreeNode* right = lca(root->right, p, q);
 
         if(!left) return right;
         else if(!right) return left;
